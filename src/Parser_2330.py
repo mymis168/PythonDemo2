d3207686eceb5ebe12ed0d847f4151974981b24b
michael <mymis168@gmaile.com>
2026-02-25 15:37:13 +0800

    修改第二版標頭

--- src/Parser_2330.py
+++ src/Parser_2330.py
@@ -21,7 +21,7 @@ def get_google_finance_price(ticker):
         price = soup.find("div", {"class": "fxKbKc"}).text
         name = soup.find("div", {"class": "zzDege"}).text
         
-        print(f"股票爬蟲 1.0版")
+        print(f"股票爬蟲 2.0版")
         print(f"股票名稱: {name}")
         print(f"即時股價: {price}")
     else:
